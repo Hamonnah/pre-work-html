33b752d0ebca1262efef780fcf391336aaff6c3b
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2021-03-10 20:40:48 +0100

    sum

---
+++ 29-js-sum/js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 3;
+let secondNumber = 5;
+
+function multiply(firstNumber, secondNumber) {
+    let result = firstNumber * secondNumber;
+
+    return result;
+}
+
+console.log(multiply(firstNumber, secondNumber));
